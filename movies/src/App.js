191acbbf4c73138960e7b69cbaba52e940950243
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 11:09:32 +0200

    applied changes

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Component/Header/Header';
 import SubNav from './Component/Header/SubNav';
 import Results from './Component/Results/Results';
 import './Component/App/App.css';
+import requests from './Component/Requests/requests';
 
 
 
 function App() {
+
+  const [selectedOption, setSelectedOption] = useState(requests.fetchTrending); 
+
   return (
     <div className="app">
       <Header />
-      <SubNav />
-      <Results />
+      <SubNav  setSelectedOption={setSelectedOption}/>
+      <Results  selectedOption={selectedOption}/>
     </div>
   );
 }
