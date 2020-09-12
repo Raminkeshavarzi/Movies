bf7098839fd5dab833b155774cc57a370b64fcba
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 09:46:02 +0200

    added the useState, useEffect, and get the request from API by using axios library

--- movies/src/Component/Results/Results.js
+++ movies/src/Component/Results/Results.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import VideoCard from '../VideoCard/VideoCard'
+import axios from '../axios/axios'; //=> Library to handle API need to use more
+import Requests from '../Requests/requests';
 import './Results.css'
 
-class Results extends React.Component{
-    render() {
-        return(
-            <div className="result">
-                <VideoCard />
-                <VideoCard />
-                <VideoCard />
-                <VideoCard />
-                <VideoCard />
-                <VideoCard />
-                <VideoCard />
-            
-            </div>
-        )
-    }
+function Results () {
+    {/*Use React hooks to create an Movies array*/}
+    const [movies, setMovies] = useState([]); 
+   
+    // add API to movies 
+    useEffect(() => {
+        // run this code once when load the results component loads/mounts
+        async function fetchData() {
+            const request = await axios.get(Requests.fetchActionMovies);
+            setMovies(request.data.results)
+        }
+
+        fetchData();
+    }, [])
+
+
+    return(
+        <div className="result">
+            {/* Maping through an Movies array to return Video component */}
+            {movies.map((movie)=>( 
+                <VideoCard movie={movie}/>
+            ))}
+        </div>
+    );
 }
 
 export default Results;
