173c2948987022ad0375264b0586652051f7cca8
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 09:13:02 +0200

    added the fetch for each movie

---
+++ movies/src/Component/Request/Request.js
@@ -0,0 +1,17 @@
+const API_KEY ="9b75c3415a4386f95b0b7aca323c47e1"
+
+
+export default {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchMystery: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    fetchSciFi: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    fetchWestern: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    fetchAnimation: `/discover/movie?api_key=${API_KEY}&with-genres=16`,
+    fetchTv: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+}
+
