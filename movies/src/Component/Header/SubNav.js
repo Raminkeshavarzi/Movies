8e42eb0eaf46dcd76574eb9ae0731b181d683173
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 08:21:32 +0200

    added the nav and its UI

--- movies/src/Component/Header/SubNav.js
+++ movies/src/Component/Header/SubNav.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './SubNav.css'
 
 class SubNav extends React.Component{
     render() {
         return(
-            <div>
-                <h1>I am nav</h1>
+            <div className="nav">
+                <h2>Trending</h2>
+                <h2>Top Rated</h2>
+                <h2>Action</h2>
+                <h2>Comedy</h2>
+                <h2>Horro</h2>
+                <h2>Romance</h2>
+                <h2>Mystery</h2>
+                <h2>Sci-Fi</h2>
+                <h2>Western</h2>
+                <h2>Animation</h2>
+                <h2>Movie</h2>
             </div>
         )
     }
