9c95fe1bb2f77a8e6635eab4d73167f66b2c4b4b
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 08:00:32 +0200

    added the SubNav

---
+++ movies/src/Component/Header/SubNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SubNav extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>I am nav</h1>
+            </div>
+        )
+    }
+}
+
+export default SubNav;
