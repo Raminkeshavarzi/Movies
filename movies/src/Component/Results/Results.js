2967994e6efe995b16fee6cc7840312c7f9cd03f
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 08:57:03 +0200

    added the result html and css

--- movies/src/Component/Results/Results.js
+++ movies/src/Component/Results/Results.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import VideoCard from '../VideoCard/VideoCard'
 import './Results.css'
 
 class Results extends React.Component{
     render() {
         return(
             <div className="result">
-                
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+                <VideoCard />
+            
             </div>
         )
     }
