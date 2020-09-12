895a0c45443351a6a8e3a391af86fe13526ddb80
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 08:57:27 +0200

    added the Video card html and css

---
+++ movies/src/Component/VideoCard/VideoCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './VideoCard.css'
+
+
+function VideoCard ({movie}) {
+    return <div className="videoCard">
+        <h1>Hello</h1>
+        <img src="./IMG_6933.jpeg" alt=""/>
+        <p>Movie overview</p>
+        <h2>Movie title</h2>
+        <p>Number of likes</p>
+    </div>
+}
+
+export default VideoCard;
