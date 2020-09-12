720ff52c6d32040779bf8bbade0c3c318f4f0f92
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 10:17:13 +0200

    added the results from api to the page

--- movies/src/Component/VideoCard/VideoCard.js
+++ movies/src/Component/VideoCard/VideoCard.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './VideoCard.css'
+import  TextTruncate from 'react-text-truncate';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 
+
+const base_url = 'https://image.tmdb.org/t/p/original/'
 
 function VideoCard ({movie}) {
     return <div className="videoCard">
-        <h1>Hello</h1>
-        <img src="./IMG_6933.jpeg" alt=""/>
-        <p>Movie overview</p>
-        <h2>Movie title</h2>
-        <p>Number of likes</p>
+        <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="Movie Poster"/>
+        <TextTruncate 
+            line={2}
+            element='p'
+            truncateText='...'
+            textTruncateChild = {<a href="#"> Read On</a>}
+            text={movie.overview}
+        />
+        <h2>{movie.title || movie.original_name}</h2>
+        <p className="vidoCard_stats">
+            {movie.media_type && `${movie.media_type} •`}
+            {movie.release_date || movie.first_air_date} •
+            <ThumbUpIcon /> {' '}
+            {movie.vote_count}
+        </p>
     </div>
 }
 
