9a1bbe27766d8591adb3a640d6c80026ea7f6565
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 08:33:02 +0200

    added results component

---
+++ movies/src/Component/Results/Results.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Results.css'
+
+class Results extends React.Component{
+    render() {
+        return(
+            <div className="result">
+                
+            </div>
+        )
+    }
+}
+
+export default Results;
