c7d97ee3ac12cde98e35e246a2108a0c715d23f3
RaminKeshavarzi <RaminKeshavarzi@yahoo.com>
2020-09-12 07:39:57 +0200

    added the header section

--- movies/src/Component/Header/Header.js
+++ movies/src/Component/Header/Header.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import './Header.css'
+
+// Material UI Icons
+import HomeIcon from '@material-ui/icons/Home';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 
 class Header extends React.Component{
     render() {
         return(
-            <div>
-                <h1>I am header</h1>
+            <div className="header">
+                <div className="Header_icons">
+                    <div className="Header_icon Header_iconActive">
+                        <HomeIcon />
+                        <p>Home</p>
+                    </div>
+
+                    <div className="Header_icon">
+                        <LiveTvIcon />
+                        <p>Live Show</p>
+                    </div>
+
+                    <div className="Header_icon">
+                        <FlashOnIcon /> 
+                        <p>Trending</p>
+                    </div>
+
+                    <div className="Header_icon">
+                        <VideoLibraryIcon />
+                        <p>Collections</p>
+                    </div>
+
+                    <div className="Header_icon">
+                        <SearchIcon />
+                        <p>Search</p>
+                    </div>
+
+                    <div className="Header_icon">
+                        <PersonOutlineIcon />
+                        <p>Account</p>
+                    </div>
+                </div>
+
+                <img src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png" alt=""/>
             </div>
         )
     }
